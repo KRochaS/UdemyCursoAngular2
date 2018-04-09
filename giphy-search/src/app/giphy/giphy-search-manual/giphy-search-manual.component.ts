58d93129c48cb790f2dae477b011e169bd4fef2b
KRochaS <karinerocha.s@hotmail.com>
2018-04-08 21:49:18 -0300

    giphy search

---
+++ giphy-search/src/app/giphy/giphy-search-manual/giphy-search-manual.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-giphy-search-manual',
+  templateUrl: './giphy-search-manual.component.html',
+})
+export class GiphySearchManualComponent {
+    constructor() {
+
+    }
+
+    OnInit() {
+
+    }
+
+}
+
