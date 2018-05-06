9d023aaa71f61d347cd3d64daaf02b63ccafe388
KRochaS <karinerocha.s@hotmail.com>
2018-05-06 17:54:46 -0300

    testes unitários

--- giphy-search/src/app/giphy/giphy-search.service.ts
+++ giphy-search/src/app/giphy/giphy-search.service.ts
@@ -19,7 +19,11 @@ export class GiphySearchService {
 
 
 	pesquisarGiphy(limit: string, term: string): Observable<Response> {
-		const url = 'https://api.giphy.com/v1/gifs/search?q=' + term + '&api_key=dc6zaTOxFJmzC&limit=' + limit;
+		const url = this.getUrl(limit, term);
 		return this.http.get(url);
+	}
+
+	getUrl(limit: string, term: string): string {
+		return 'https://api.giphy.com/v1/gifs/search?q=' + term + '&api_key=dc6zaTOxFJmzC&limit=' + limit;
 	}
 }
---
+++ giphy-search/src/app/giphy/test/giphy-search.service.spec.ts
@@ -0,0 +1,55 @@
+import { GiphySearchService } from "../giphy-search.service";
+
+fdescribe('giphy-search.service.spec.ts', () => {
+
+    // no [] vai colocar os metódos que quer mockar
+    // que o obj precisa responder caso seja chamado
+    const httpMock = jasmine.createSpyObj('Http', ['get']);
+    let service: GiphySearchService;
+    beforeEach(() => {
+
+        service: new GiphySearchService(null);
+        // onde colocar inicializações que vão valer para todos os testes
+        // que estiverem dentro do describe
+
+        // para todos os each sempre vai criar uma instancia do objeto
+
+    })
+
+    // f na frente do it idica que quer que o
+    //  teste automatizado seja apenas nele
+    // ou f na frente do describe indica que todos os 
+    //metodos it dentro do describe vão rodar
+    // fit('2 deveria ser igual a 2', () => {
+    //     // entrada
+    //     let a = 0;
+
+    //     // processamento
+
+    //     a = 0 + 1;
+
+    //     // saída
+    //     expect(a).toBe(2);
+
+
+    // });
+
+    it('pesquisarGiphy', () => {
+        service.pesquisarGiphy('1', 'happy');
+        
+        expect(httpMock.get).toHaveBeenCalled();
+
+        
+        });
+
+    // testando o método pesquisarGiphy
+    it('getUrl', () => {
+
+        const url = service.getUrl('2', 'happy');
+
+        expect(url)
+        .toBe('https://api.giphy.com/v1/gifs/search?q=happy&api_key=dc6zaTOxFJmzC&limit=2');
+    });
+
+    
+});
