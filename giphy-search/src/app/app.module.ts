0e81f73e92522faa023bb3c5b30ebb1ceea2f893
KRochaS <karinerocha.s@hotmail.com>
2018-11-12 16:11:55 -0200

    Fix

--- giphy-search/src/app/app.module.ts
+++ giphy-search/src/app/app.module.ts
@@ -5,6 +5,7 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { GiphySearchManualComponentModule } from './giphy/giphy-search-manual/giphy-search-manual.module';
 import { AppRoutingModule } from './app-routing.module';
+import { ContatoModule } from './contato/contato.module';
 
 
 @NgModule({
@@ -14,6 +15,7 @@ import { AppRoutingModule } from './app-routing.module';
 	imports: [
 		BrowserModule,
 		GiphySearchManualComponentModule,
+		ContatoModule,
 		AppRoutingModule
 	],
 	providers: [],
